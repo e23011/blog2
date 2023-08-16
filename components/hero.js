c1c88f3634f1df19d8c5a1001d575d8418965b9a
e23011 <e23011@std.it-college.ac.jp>
2023-08-16 20:49:45 +0900

    4.3

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,11 @@
 import styles from 'styles/hero.module.css'
 
 const Hero = ({title, subtitle, imageOn = false}) =>(
+  <div className={styles.flexContainer}>
   <div classname={styles.text}>
     <h1 classname={styles.title}>{title}</h1>
     <p classname={styles.subtitle}>{subtitle}</p>
+  </div>
     { imageOn && <figure>[画像]</figure> }
   </div>
  )
