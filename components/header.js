998c164cdfeaba80e7ad652ec86e8d6e26864078
e23011 <e23011@std.it-college.ac.jp>
2023-08-16 20:42:47 +0900

    4.2

--- components/header.js
+++ components/header.js
@@ -1,9 +1,12 @@
 import Logo from 'components/logo'
 import Nav from 'components/nav'
+import styles from 'styles/header.module.css'
 const Header = () => (
   <header>
+    <div className={styles.flexContainer}>
     <Logo boxOn />
     <Nav/>
+    </div>
   </header>
 )
 export default Header
